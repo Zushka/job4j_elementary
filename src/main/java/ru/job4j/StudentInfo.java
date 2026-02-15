c0683c05b2109bda76fd372efef75bb296c6782c
NAZAR <mrzushka@gmail.com>
2026-02-15 20:39:11 +0200

    6. Checkstyle [#4123 #394684]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[]args) {
+    public static void main(String[]args) {
         System.out.println("Nazar");
         System.out.println("16.08.2001");
     }
