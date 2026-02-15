7505ec0e442fddab12a624978a2236000315f897
NAZAR <mrzushka@gmail.com>
2026-02-15 19:24:18 +0200

    4.1. Разбор первой программы. [#159033 #394663]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[]args) {
+        System.out.println("Nazar");
+        System.out.println("16.08.2001");
+    }
+}
